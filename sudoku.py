a0f100fae7b9a547acb6052ceedc0476f045705e
Jan Gwara <janekgwara13@gmail.com>
2024-09-08 19:41:42 +0200

    the matrices are generated but without uniqueness of numbers

--- sudoku.py
+++ sudoku.py
@@ -2,20 +2,27 @@ import math
 import random
 import numpy as np
 
-sudokuGrid = [[1, 2, 3, 0, 0, 0, 0, 0, 0], 
-[4, 5, 6, 0, 0, 0, 0, 0, 0], 
-[7, 8, 9, 0, 0, 0, 0, 0, 0], 
+sudokuGrid = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0], 
 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-[0, 0, 0, 0, 0, 0, 0, 0, 0]]
+[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
 for row in range(0, 3):
     for column in range(0, 3):
-        row = np.random.randint(10, size=(3)) 
+        sudokuGrid[row, column:column + 1] = np.random.randint(1, 10, 1)
 
+for row in range(3, 6):
+    for column in range(3, 6):
+        sudokuGrid[row, column:column + 1] = np.random.randint(1, 10, 1)
+        
+for row in range(6, 9):
+    for column in range(6, 9):
+        sudokuGrid[row, column:column + 1] = np.random.randint(1, 10, 1)
 
 
 for row in range(0, 9):
